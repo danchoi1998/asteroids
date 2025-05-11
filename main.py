90d4978388d98cd714fb3d230ab6a6c8b63a46ed
danchoi1998 <danielsomangchoi@gmail.com>
2025-05-11 15:24:29 -0400

    added quittable pygame screen to main() function

--- main.py
+++ main.py
@@ -6,10 +6,21 @@ from constants import *
 
 def main():
     pygame.init
+
     print("Starting Asteroids!")
     print(f"""Screen width: {SCREEN_WIDTH}
     Screen height: {SCREEN_HEIGHT}
     """)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while 1 == 1:
+        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+            return
+        pygame.display.update()
+
 
 if __name__ == "__main__":
     main()
